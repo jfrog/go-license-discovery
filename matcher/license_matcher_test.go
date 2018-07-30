f569ce5cde7bd325f1f48c0f3c98da05b54480b5
Chen Keinan <chenk@jfrog.com>
2018-07-31 00:39:54 +0300

    Add license matcher test

---
+++ matcher/license_matcher_test.go
@@ -0,0 +1,87 @@
+package matcher
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"io/ioutil"
+)
+
+const (
+	NoLicense = "no licenes"
+)
+
+func TestInitLicenseMatcherWithNoLicensesDB(t *testing.T) {
+	err:=InitLicenseMatcher("unknown folder")
+	if err == nil{
+		t.Fatal(err)
+	}
+}
+
+func TestInitLicenseMatcherWithLicensesDB(t *testing.T) {
+	err:=InitLicenseMatcher("./licenses/")
+	if err != nil{
+		t.Fatal(err)
+	}
+}
+
+func TestMatchLicenseTxtNoLicenseFile(t *testing.T){
+	err:=InitLicenseMatcher("./licenses/")
+	if err != nil{
+		t.Fatal(err)
+	}
+	lics:=MatchLicenseTxt(NoLicense)
+	assert.True(t,len(lics) == 0 || lics[0]=="Unknown")
+
+}
+
+func TestMatchLicenseTxtWithClassifier(t *testing.T){
+	err:=InitLicenseMatcher("./licenses/")
+	if err != nil{
+		t.Fatal(err)
+	}
+	f,err:=os.Open("./fixtures/Multi_LICENSE.txt")
+	if err != nil{
+		t.Fatal(err)
+	}
+	data,err:=ioutil.ReadAll(f)
+	if err != nil{
+		t.Fatal(err)
+	}
+	lics:=MatchLicenseTxt(string(data))
+	assert.True(t,len(lics) == 6)
+}
+
+func TestMatchLicenseTxtWithDetector(t *testing.T){
+	err:=InitLicenseMatcher("./licenses/")
+	if err != nil{
+		t.Fatal(err)
+	}
+	f,err:=os.Open("./fixtures/Partial_LICENSE.txt")
+	if err != nil{
+		t.Fatal(err)
+	}
+	data,err:=ioutil.ReadAll(f)
+	if err != nil{
+		t.Fatal(err)
+	}
+	lics:=MatchLicenseTxt(string(data))
+	assert.True(t,len(lics) == 1)
+}
+
+func TestMatchLicenseTxtWithPom(t *testing.T){
+	err:=InitLicenseMatcher("./licenses/")
+	if err != nil{
+		t.Fatal(err)
+	}
+	f,err:=os.Open("./fixtures/PomWithLicenseAsComment.xml")
+	if err != nil{
+		t.Fatal(err)
+	}
+	data,err:=ioutil.ReadAll(f)
+	if err != nil{
+		t.Fatal(err)
+	}
+	lic:=GetPomCommentLicense(string(data))
+	assert.True(t,lic == "Apache-2.0")
+}
